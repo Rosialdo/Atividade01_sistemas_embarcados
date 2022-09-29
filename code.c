a2767dd43df6b91254dbe9481eae101bbfd97161
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-09-29 08:10:53 -0400

    Now the code runs
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- code.c
+++ code.c
@@ -3,27 +3,29 @@
 typedef enum {
     start,
     button,
-    led,
+    red_led,
     cup,
     tea,
     coffee,
+    green_led
 } state;
 
 typedef enum {
     chip,
     select_drink,
-    turn_led_on,
+    turn_red_led_on,
     cup_down,
     fill_cup,
-    user_grabs_cup
+    user_grabs_cup,
+    turn_green_led_on
 } event;
 
 state press_button() { return button; }
-state turn_wait_led_on() { return led; }
+state turn_wait_led_on() { return red_led; }
 state drop_cup() { return cup; }
 state pour_tea() { return tea; }
 state pour_coffee() { return coffee; }
-state turn_green_led_on() { return led; }
+state turn_ready_led_on() { return green_led; }
 state turn_led_off() { return start; }
 
 void print_state(state s) {
@@ -34,8 +36,8 @@ void print_state(state s) {
         case button:
             printf("button -> ");
             break;
-        case led:
-            printf("led -> ");
+        case red_led:
+            printf("red_led -> ");
             break;
         case cup:
             printf("cup -> ");
@@ -46,6 +48,9 @@ void print_state(state s) {
         case coffee:
             printf("coffee -> ");
             break;
+        case green_led:
+            printf("green_led -> ");
+            break;
         default:
             printf("ERRO\n");
             break;
@@ -54,11 +59,11 @@ void print_state(state s) {
 
 void transition(event e) {
     static state s = start;
-    char user_input[2];
+    char user_input;
 
     while (1) {
         print_state(s);
-        if (e == user_grabs_cup) e = chip;
+        if (e == turn_green_led_on) e = chip;
 
         switch (s) {
             case start:
@@ -66,31 +71,30 @@ void transition(event e) {
                 s = press_button();
                 break;
             case button:
-                if (e == select_drink) e = turn_led_on;
+                if (e == select_drink) e = turn_red_led_on;
                 s = turn_wait_led_on();
                 break;
-            case led:
-                if (e == turn_led_on) {
-                    e = cup_down;
-                    s = drop_cup();
-                } else if (e == user_grabs_cup) {
-                    e = chip;
-                    s = turn_led_off();
-                }
+            case red_led:
+                if (e == turn_red_led_on) e = cup_down;
+                s = drop_cup();
                 break;
             case cup:
                 if (e == cup_down) e = fill_cup;
-                fgets(user_input, 2, stdin);
-                if (user_input[0] == 'c') s = pour_coffee(); 
-                else if (user_input[0] == 't') s = pour_tea(); 
+                scanf(" %c", &user_input);
+                if (user_input == 'c') s = pour_coffee(); 
+                else if (user_input == 't') s = pour_tea(); 
                 break;
             case tea:
                 if (e == fill_cup) e = user_grabs_cup;
-                s = turn_green_led_on();
+                s = turn_ready_led_on();
                 break;
             case coffee:
                 if (e == fill_cup) e = user_grabs_cup;
-                s = turn_green_led_on();
+                s = turn_ready_led_on();
+                break;
+            case green_led:
+                if (e == user_grabs_cup) e = turn_green_led_on;
+                s = turn_led_off();
                 break;
             default:
                 printf("ERRO\n");
